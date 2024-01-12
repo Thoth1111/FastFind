a2a6a6cde21278e236d7d9e4d8acf4ae0e2ffd57
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-12 17:59:13 +0300

    Configure routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'logs/index'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :logs, :only => [:index]
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "logs#index"
 end
