90773b2cce9d3b1cd7ee7395eeee592cafd1cb71
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-13 03:28:15 +0300

    Update routing and controllers for API functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # Define my API routes
+  namespace :api, defaults: { format: 'json' } do
+    resources :logs, :only => [:index]
+  end
   
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Define my application routes 
   resources :page, :only => [:index]
 
   # Defines the root path route ("/")
