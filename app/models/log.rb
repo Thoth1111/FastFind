8309eac60ac2b0326f9c1a15e5e4e749e778593e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-13 03:29:19 +0300

    Update Log model

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,13 @@
 class Log < ApplicationRecord
+    validates :query, presence: true
+    validates :ip_address, presence: true
+    validates :count, presence: true
+    validates :query, uniqueness: { scope: :ip_address }
+
+    def self.search(query, ip_address)
+        #Create a new log if it doesn't exist, otherwise increment the existing query's count
+        log = find_or_create_by(query: query, ip_address: ip_address)
+        log.increment!(:count)
+        log
+    end
 end
