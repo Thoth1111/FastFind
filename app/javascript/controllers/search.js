0664e17b2a7fd217a8a54b954704611bfdca51b0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-13 08:35:07 +0300

    Update Javascript files

--- app/javascript/controllers/search.js
+++ app/javascript/controllers/search.js
@@ -1,47 +1,59 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.getElementById('search_bar');
-    const searchSubmit = document.getElementById('search_form');
-    let timeout = null;
+  const searchInput = document.getElementById('search_bar');
+  const searchSubmit = document.getElementById('search_form');
+  const matchesList = document.getElementById('matches');
+  let timeout;
 
-    searchInput.addEventListener('input', () => {
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            searchQuery(searchInput.value);
-        }, 1000);
-    });
-
-    searchSubmit.addEventListener('submit', (e) => {
-        e.preventDefault();
-        saveQuery(searchInput.value);
-    });
-
-    function searchQuery(query) {
-       $.ajax({
-        url: process.env.ANALYTICS_URL,
-        method: 'GET',
-        dataType: 'json',
-        data: {query: query},
-        success: (data) => {
-            console.log(data);
-        },
-        error: (err) => {
-            console.log(err);
-        }
-       })
+  function updateMatches(matches) {
+    for (const match of matches) {
+      const item = document.createElement('li');
+      item.innerText = match.query;
+      matchesList.appendChild(item);
     }
+  }
 
-    function saveQuery(query) {
-        $.ajax({
-            url: process.env.SAVE_QUERY_URL,
-            method: 'POST',
-            dataType: 'json',
-            data: {query: query},
-            success: (res) => {
-                console.log(res);
-            },
-            error: (err) => {
-                console.log(err);
-            }
-        })
-    }
+  function searchQuery(query) {
+    fetch(process.env.ANALYTICS_URL, {
+      method: 'GET',
+      data: { query },
+    })
+      .then((res) => {
+        updateMatches(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function saveQuery(query) {
+    fetch(process.env.SAVE_QUERY_URL, {
+      method: 'POST',
+      body: JSON.stringify({ query }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  searchInput.addEventListener('input', () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      searchQuery(searchInput.value);
+    }, 1000);
+  });
+
+  searchSubmit.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const query = searchInput.value;
+    if (query === '') {
+        alert('field cannot be empty')
+    } else {;
+        saveQuery(query);
+    }});
 });
